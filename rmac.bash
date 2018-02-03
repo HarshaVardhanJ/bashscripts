9471c4fb3de0042c67ba14df0611391a11b88086
Henri Cattoire <henricattoire0@gmail.com>
2018-02-03 16:20:13 +0100

    randomMAC.bash

--- rmac.bash
+++ rmac.bash
@@ -6,7 +6,7 @@
 # Sniffing up the old MAC
 OLD="$(ifconfig userInterface | grep ether | awk -F " " '{print $2}')"
 # Main
-openssl rand -hex 1 | tr '[:lower:]' '[:upper:]' | xargs echo "obase=2;ibase=16;" | bc | cut -c1-6 | sed 's/$/00/' | xargs echo "obase=16;ibase=2;" | bc | sed "s/$/:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/" | xargs sudo ifconfig "$userInterface" ether
+openssl rand -hex 1 | tr '[:lower:]' '[:upper:]' | xargs echo "obase=2;ibase=16;" | bc | cut -c1-6 | sed 's/$/00/' | xargs echo "obase=16;ibase=2;" | bc | sed "s/$/:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/" | xargs sudo ifconfig userInterface ether
 sudo ifconfig userInterface down
 sleep 2
 sudo ifconfig userInterface up
