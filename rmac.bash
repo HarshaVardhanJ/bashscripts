c3fa9e934ce82b303c13bbe9beb5ad18f904b7a4
Henri Cattoire <henricattoire0@gmail.com>
2018-02-03 16:17:21 +0100

    randomMAC.bash

--- rmac.bash
+++ rmac.bash
@@ -3,8 +3,7 @@
 # licensed under CC
 # Author: Henri Cattoire
 
-
-# Sniffing up the old MAC (only useful for option a and b)
+# Sniffing up the old MAC
 OLD="$(ifconfig userInterface | grep ether | awk -F " " '{print $2}')"
 # Main
 openssl rand -hex 1 | tr '[:lower:]' '[:upper:]' | xargs echo "obase=2;ibase=16;" | bc | cut -c1-6 | sed 's/$/00/' | xargs echo "obase=16;ibase=2;" | bc | sed "s/$/:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/" | xargs sudo ifconfig "$userInterface" ether
