4f40c06f7152606d63339e8a9bbf010dd0611114
Henri Cattoire <henricattoire0@gmail.com>
2018-02-03 16:27:49 +0100

    specifiedMAC.bash

--- smac.bash
+++ smac.bash
@@ -1,12 +1,12 @@
 #! /bin/bash
-# Set specific MAC address (you change en1 [wifi] to whatever interface you want and neMac to the wanted MAC address)
+# Set specific MAC address (you change en1 [wifi] to whatever interface you want and 00:50:56:64:d1:6a to the wanted MAC address)
 # licensed under CC
 # Author: Henri Cattoire
 
 # Sniffing up the old MAC
 OLD="$(ifconfig en1 | grep ether | awk -F " " '{print $2}')"
 # Main
-sudo ifconfig en1 ether newMac
+sudo ifconfig en1 ether 00:50:56:64:d1:6a
 sudo ifconfig en1 down
 sleep 2
 sudo ifconfig en1 up
