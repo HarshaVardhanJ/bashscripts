e4cd563a7b624eff909b0f6c19ad0a6a3e8a1113
Henri Cattoire <henricattoire0@gmail.com>
2018-02-03 16:50:34 +0100

    cleanDir

--- cleandir.bash
+++ cleandir.bash
@@ -7,7 +7,7 @@
 userName="$(id -un)"
 # (1) Removing all files within that directory (uncomment this line of code when using option 2)
 rm /Users/"$userName"/Downloads/*
-# (2) You can also uncomment the following block of code if you want user confimation
+# (2) You can also uncomment the following block of code if you want user confirmation
 #read -p "Are you sure (Y/N)? " -n 1 -r
 #if [[ $REPLY =~ ^[Yy]$ ]]
 #then
