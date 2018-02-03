978e423d37f8ef0455432913040180c193b96947
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-03 18:25:03 +0100

    nmap.bash

---
+++ nmap.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Makes and saves .nse scripts (or .lua libraries) in the right directory
+# Author: Henri Cattoire
+
+# Going to the right directory
+cd /usr/local/share/nmap/scripts # option for the scripts
+#cd /usr/local/share/nmap/nselib # option for the libraries
+# Main
+curl https://svn.nmap.org/nmap/scripts/ajp-auth.nse >> ajp.auth.nse
+
