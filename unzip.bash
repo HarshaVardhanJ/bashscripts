f46951a5ca82461586bb6fc30a71d8358b1b2cce
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-03 17:38:08 +0100

    unzip.bash

--- unzip.bash
+++ unzip.bash
@@ -1,13 +1,12 @@
 #! /bin/bash
 # Quickly unzip a file located in the downloads directory (or any other directory)
-# licensed under CC
 # Author: Henri Cattoire
 
 # Sniffing up the active user and going to the specified directory
 userName="$(id -un)"
 cd /Users/"$userName"/Downloads
 # Main
-userFile="$(mdfind -onlyin /Users/"$userName"/Downloads .zip)"
+userFile="$(find ./ -name \*.zip -exec unzip {} \;)"
 unzip $userFile
 # Cleaning everything up 
 rm -r *.zip > /dev/null 2>&1
