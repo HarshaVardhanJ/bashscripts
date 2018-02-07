7e4766210c776ebeb8dcdc37615ee1d4bbda72f3
Henri Cattoire <henricattoire0@gmail.com>
2018-02-07 16:05:54 +0100

    nmap.bash

--- nmap.bash
+++ nmap.bash
@@ -5,6 +5,6 @@
 # Going to the right directory
 cd /usr/local/share/nmap/scripts # option for the scripts
 #cd /usr/local/share/nmap/nselib # option for the libraries
-# Main (URL and name should be changed to the script you want)
+# Main (URL and name are to be changed by the user)
 curl https://svn.nmap.org/nmap/scripts/ajp-auth.nse >> ajp.auth.nse
 
