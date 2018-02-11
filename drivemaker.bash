5e340effa295dd5c61113a1fd170e9858dbd90b8
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-11 19:04:42 +0100

    drivemaker.bash

--- drivemaker.bash
+++ drivemaker.bash
@@ -10,10 +10,14 @@ rm -r "$DIR" > /dev/null 2>&1
 mkdir "$DIR"
 cd "$DIR"
 # Make proper variable of your disk (please change chosenDisk to your disk, ex. disk3)
-DISK="/dev/chosenDisk"
+DISK="/dev/disk3"
 # Downloading the ISO (change the url to download the ISO you want)
 curl -L -O https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-"$D_VERSION"-amd64-netinst.iso
 ISOFILE="$(ls | grep .iso)"
+# Converting ISO file to IMG file (only useful to make a liveUSB, comment these lines out when going for full install)
+TEMPFILE="$(ls | grep .iso | sed "s@.iso@.img@g")"
+hdiutil convert -format UDRW -o "$TEMPFILE" "$ISOFILE"
+IMGFILE="$(ls | grep .img.dmg)"
 # Preparing the disk
 echo
 echo "Erasing your disk and setting its format to MS-DOS, you have 5 second to abort this by hitting command and C"
@@ -25,9 +29,10 @@ diskutil unmountDisk "$DISK"
 echo
 echo "Making your bootable drive, this can take up to 60 minutes"
 # Use this command if you didn’t install homebrew and pv yet (make sure to comment this command when using the command below!)
-dd if="$ISOFILE" of="$DISK" bs=4m
+#dd if="$IMGFILE" of="$DISK" bs=4m
 # Uncomment this command when you want to show progress (homebrew [https://brew.sh/] and pv [brew install pv] are needed) 
-#dd if="$ISOFILE" | pv | dd of="$DISK" bs=4m
-# Cleaning up the application
+dd if="$IMGFILE" | pv | dd of="$DISK" bs=4m
+# Cleaning up the application and ejecting the disk
 rm -r "$DIR"
+diskutil eject "$DISK"
 echo "Quitting the application, hope to see you again soon!"  
