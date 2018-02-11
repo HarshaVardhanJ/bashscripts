ea61a949efa383c92f25d546bf6b63d309a2b3ce
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-11 16:07:14 +0100

    vpnbook.bash

--- vpnbook.bash
+++ vpnbook.bash
@@ -1,13 +1,14 @@
 #! /bin/bash
-# Simple bash script to start up openvpn and run it (you can install openvpn using homebrew [brew install openvpn])
+# Simple bash script to start up openvpn and run it with vpnbook (you can install openvpn using homebrew [brew install openvpn])
 # Author: Henri Cattoire
 # TO BE CHANGED: username (to find out yours, just use this command: id -un)
 
-# Colors 
+# Variables
+VPN="vpnbook-fr1-tcp80.ovpn" # Change the value of the variable to the desired VPN from https://www.vpnbook.com/freevpn
 GREEN=$'\e[1;32m'
 EXIT=$'\e[0m'
 # Moving to directory where our VPN’s are stored  (make this directory before using the script with this command: mkdir /Users/username/.openvpn-script )
-DIR="/Users/username/.openvpn-script" #
+DIR="/Users/henricattoire/.openvpn-script" #
 cd "$DIR"
 rm -rf freevpn  > /dev/null 2>&1 # Makes sure that there will not be duplicates
 curl -O https://www.vpnbook.com/freevpn  > /dev/null 2>&1
@@ -17,6 +18,8 @@ PWD="$(cat freevpn | grep -m 1 "Password:" | sed "s@<li>Password: <strong>@@g" |
 # Main
 echo "Current username: ${GREEN}$UN${EXIT}"
 echo "Current password: ${GREEN}$PWD${EXIT}"
-openvpn /Users/username/.openvpn-script/vpnbook-fr1-tcp80.ovpn # Be sure to move all your VPNbook files to the /Users/username/.openvpn-script directory using the mv command
+echo
+echo "${GREEN}You chose to connect to "$VPN" ...${EXIT}"
+openvpn /Users/henricattoire/.openvpn-script/"$VPN" # Be sure to move all your VPNbook files to the /Users/username/.openvpn-script directory using the mv command
 # Cleaning up the script
 rm -rf freevpn  > /dev/null 2>&1
