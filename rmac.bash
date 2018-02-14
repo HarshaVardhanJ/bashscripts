b470be329f1db16e43b0fcd773c86fa70c04bea3
Henri Cattoire <henricattoire0@gmail.com>
2018-02-14 14:47:45 +0100

    rmac.bash

--- rmac.bash
+++ rmac.bash
@@ -2,14 +2,16 @@
 # Set random MAC address (you can change en1 [wifi] to whatever interface you want)
 # Author: Henri Cattoire
 
-# Sniffing up the old MAC
-OLD="$(ifconfig en1 | grep ether | awk -F " " '{print $2}')"
+# Variables
+INTERFACE="en1"
+# Sniffing up the old MAC (only useful for option a and b)
+OLD="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
 # Main
-openssl rand -hex 1 | tr '[:lower:]' '[:upper:]' | xargs echo "obase=2;ibase=16;" | bc | cut -c1-6 | sed 's/$/00/' | xargs echo "obase=16;ibase=2;" | bc | sed "s/$/:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/" | xargs sudo ifconfig en1 ether
-sudo ifconfig en1 down
+openssl rand -hex 1 | tr '[:lower:]' '[:upper:]' | xargs echo "obase=2;ibase=16;" | bc | cut -c1-6 | sed 's/$/00/' | xargs echo "obase=16;ibase=2;" | bc | sed "s/$/:$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//' | tr '[:lower:]' '[:upper:]')/" | xargs sudo ifconfig "$INTERFACE" ether
+sudo ifconfig "$INTERFACE" down
 sleep 2
-sudo ifconfig en1 up
-OUTPUT="$(ifconfig en1 | grep ether | awk -F " " '{print $2}')"
+sudo ifconfig "$INTERFACE" up
+OUTPUT="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
 echo
 echo "Current MAC: "$OUTPUT""
 echo "Old MAC: "$OLD""
