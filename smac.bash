0065df5d3aa62b35fc47aed1e5aa5d6b98f1172c
Henri Cattoire <henricattoire0@gmail.com>
2018-02-14 14:40:12 +0100

    smac.bash

--- smac.bash
+++ smac.bash
@@ -2,14 +2,17 @@
 # Set specific MAC address (you can change en1 [wifi] to whatever interface you want and 00:50:56:64:d1:6a to the wanted MAC address)
 # Author: Henri Cattoire
 
+# Variables
+MAC="00:50:56:64:d1:6a"
+INTERFACE="en1"
 # Sniffing up the old MAC
-OLD="$(ifconfig en1 | grep ether | awk -F " " '{print $2}')"
+OLD="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
 # Main
-sudo ifconfig en1 ether 00:50:56:64:d1:6a
-sudo ifconfig en1 down
+sudo ifconfig "$INTERFACE" ether "$MAC"
+sudo ifconfig "$INTERFACE" down
 sleep 2
-sudo ifconfig en1 up
-OUTPUT="$(ifconfig en1 | grep ether | awk -F " " '{print $2}')"
+sudo ifconfig "$INTERFACE" up
+OUTPUT="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
 echo
 echo "Current MAC: "$OUTPUT""
 echo "Old MAC: "$OLD""
