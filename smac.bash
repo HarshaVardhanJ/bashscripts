654ea38c6248c5f0d21960bc56a23812a5660738
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-19 22:28:13 +0100

    smac.bash

--- smac.bash
+++ smac.bash
@@ -2,22 +2,17 @@
 # Set specific MAC address (you can change en1 [wifi] to whatever interface you want and specify the MAC address with a -m flag [sh smac.bash -m 00:50:56:64:d1:6a])
 # Author: Henri Cattoire
 
-# Flags
-while getopts ":h" opt; do
-  case $opt in
-    h)
-      echo "Usage: sh $0 [-m <mac>]" 1>&2
-      exit 1
-  esac
-done
-while getopts ":m:" opt; do
-  case $opt in
+# Flag
+while getopts ":m:" mac; do
+  case $mac in
     m)
       m=${OPTARG}
+      ;;
   esac
 done
 # Variables
 MAC="${m}"
+echo "$MAC"
 INTERFACE="en1"
 # Sniffing up the old MAC
 OLD="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
@@ -30,3 +25,4 @@ OUTPUT="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
 echo
 echo "Current MAC: "$OUTPUT""
 echo "Old MAC: "$OLD""
+
