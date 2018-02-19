f944e3e5b55f51cece670eb1d669ee0120a42f8f
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-19 20:55:25 +0100

    nmap.bash
    added -n flag to specify the name of the script you want to add

--- nmap.bash
+++ nmap.bash
@@ -2,9 +2,18 @@
 # Makes and saves .nse scripts (or .lua libraries) in the right directory
 # Author: Henri Cattoire
 
-# Going to the right directory
-cd /usr/local/share/nmap/scripts # option for the scripts
-#cd /usr/local/share/nmap/nselib # option for the libraries
-# Main (URL and name are to be changed by the user)
-curl https://svn.nmap.org/nmap/scripts/ajp-auth.nse >> ajp.auth.nse
-
+while getopts ":n:" opt; do 
+  case $opt in
+    n)
+      # Variables
+      SCRIPT="$OPTARG" # or LIB="$OPTARG" for libraries
+      # Going to the right directory
+      cd /usr/local/share/nmap/scripts # or cd /usr/local/share/nmap/nselib for libraries
+      # Main
+      curl https://svn.nmap.org/nmap/scripts/"$SCRIPT" >> "$SCRIPT" # or curl https://svn.nmap.org/nmap/nselib/"$LIB" >> "$LIB" for libraries
+      CHECK="$(find "$SCRIPT")"
+      echo "FOLLOWING FILE WAS ADDED: "$SCRIPT"" # or echo "FOLLOWING FILE WAS ADDED: "$LIB""
+      exit 1
+      ;;
+   esac
+done
