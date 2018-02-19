e1506680b4a522eea2652b4df6f4bde9ae8ea1b7
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-19 22:00:48 +0100

    nmap.bash
    updated for a cleaner script

--- nmap.bash
+++ nmap.bash
@@ -1,19 +1,20 @@
 #! /bin/bash
-# Makes and saves .nse scripts (or .lua libraries) in the right directory
+# Makes and saves .nse scripts (or .lua libraries) in the right directory (use the -n flag with as argument the name of the script/lib)
 # Author: Henri Cattoire
 
+# Flag
 while getopts ":n:" opt; do 
   case $opt in
     n)
-      # Variables
-      SCRIPT="$OPTARG" # or LIB="$OPTARG" for libraries
-      # Going to the right directory
-      cd /usr/local/share/nmap/scripts # or cd /usr/local/share/nmap/nselib for libraries
-      # Main
-      curl https://svn.nmap.org/nmap/scripts/"$SCRIPT" >> "$SCRIPT" # or curl https://svn.nmap.org/nmap/nselib/"$LIB" >> "$LIB" for libraries
-      CHECK="$(find "$SCRIPT")"
-      echo "FOLLOWING FILE WAS ADDED: "$SCRIPT"" # or echo "FOLLOWING FILE WAS ADDED: "$LIB""
-      exit 1
+      n=${OPTARG}
       ;;
    esac
 done
+# Variables
+SCRIPT="${n}" # or LIB="$OPTARG" for libraries
+# Going to the right directory
+cd /usr/local/share/nmap/scripts # or cd /usr/local/share/nmap/nselib for libraries
+# Main
+curl https://svn.nmap.org/nmap/scripts/"$SCRIPT" >> "$SCRIPT" # or curl https://svn.nmap.org/nmap/nselib/"$LIB" >> "$LIB" for libraries
+CHECK="$(find "$SCRIPT")"
+echo "FOLLOWING FILE WAS ADDED: "$SCRIPT"" # or echo "FOLLOWING FILE WAS ADDED: "$LIB""
