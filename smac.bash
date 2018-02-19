ef7d6ce15d068ea951b6bde64beabd4c9613ad3c
HenriC4ttoire <henricattoire0@gmail.com>
2018-02-19 20:22:42 +0100

    smac.bash
    updated to V2 where you can just pass in the MAC as an argument to the -m flag

--- smac.bash
+++ smac.bash
@@ -1,18 +1,25 @@
 #! /bin/bash
-# Set specific MAC address (you can change en1 [wifi] to whatever interface you want and 00:50:56:64:d1:6a to the wanted MAC address)
+# Set specific MAC address (you can change en1 [wifi] to whatever interface you want and specify the MAC address with a -m flag [sh smac.bash -m 00:50:56:64:d1:6a])
 # Author: Henri Cattoire
 
-# Variables
-MAC="00:50:56:64:d1:6a"
-INTERFACE="en1"
-# Sniffing up the old MAC
-OLD="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
-# Main
-sudo ifconfig "$INTERFACE" ether "$MAC"
-sudo ifconfig "$INTERFACE" down
-sleep 2
-sudo ifconfig "$INTERFACE" up
-OUTPUT="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
-echo
-echo "Current MAC: "$OUTPUT""
-echo "Old MAC: "$OLD""
+while getopts ":m:" opt; do
+  case $opt in
+    m)
+      # Variables
+      MAC="$OPTARG"
+      INTERFACE="en1"
+      # Sniffing up the old MAC
+      OLD="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
+      # Main
+      sudo ifconfig "$INTERFACE" ether "$MAC"
+      sudo ifconfig "$INTERFACE" down
+      sleep 2
+      sudo ifconfig "$INTERFACE" up
+      OUTPUT="$(ifconfig "$INTERFACE" | grep ether | awk -F " " '{print $2}')"
+      echo
+      echo "Current MAC: "$OUTPUT""
+      echo "Old MAC: "$OLD""
+      exit 1
+      ;;
+  esac
+done
